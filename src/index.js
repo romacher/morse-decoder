3666a4649ca88ecb674b3075662c117b5f529d7c
Dmitry Kim <d.kim2009@tut.by>
2019-09-24 23:52:02 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var result = "";
+    for (var i=0; i<expr.length; i+=10) {
+        var letter_binary = expr.substr(i,10);
+        if (letter_binary == "**********") result+=" ";
+        else {
+            var letter = "";
+            for (var j=0; j<letter_binary.length; j+=2) {
+                var char_binary = letter_binary.substr(j,2);
+                if (char_binary == "00") continue;
+                else if (char_binary == "10") letter+=".";
+                     else if (char_binary == "11") letter+="-";
+                        else break;
+            }
+            result+=MORSE_TABLE[letter];
+        }
+    }
+    return result;
 }
 
 module.exports = {
