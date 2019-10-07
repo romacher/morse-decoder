3776375325cf87203272d54968fde6bdb63d7c3a
L0kka <w1llya@pm.me>
2019-10-07 18:07:14 +0300

    feat: add morse-decoder function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
     // write your solution here
+var arr = [];
+var n = "";
+var str = "";
+
+for (var i = 0; i < expr.length; j=0) {
+    for(var j = 0; j < 10; j++) {
+      n += expr[i];
+      i++;
+    }
+   arr.push(n);
+   n = "";
+ }
+
+for (i = 0; i < arr.length; i++) {
+    for(j = 0; j < 10; j++) {
+        if (arr[i][j] === '1' || arr[i][j] === '*') {
+            switch(arr[i][j+1]) {
+                case '0':
+                    n += '.';
+                    j++;
+                    break;
+                case '1':
+                    n += '-';
+                    j++;
+                    break;
+                case '*':
+                    n += ' ';
+                    j = 10;
+                    break;
+                default: break;
+            }
+        }
+    }
+    if (MORSE_TABLE[n])
+        str += MORSE_TABLE[n];
+    else str += ' ';
+    n = "";
+}
+
+return str;
 }
 
 module.exports = {
