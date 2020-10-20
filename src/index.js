547995f322916e4d9448731917a7e64f3b1f94d1
Andrey Goncharenko <goncharenko.andry@yandex.by>
2020-10-20 21:01:51 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -38,7 +38,65 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arrBig = [];
+    let arrBigSize = 10;
+
+    let arrSmall = [];
+    let arrSmallSize = 2;
+
+    let letter = '';
+    let arr = [];
+
+    while (expr) {
+        if (expr.length < arrBigSize) {
+            arrBig.push(expr);
+            break;
+        }
+        else {
+            arrBig.push(expr.substr(0, arrBigSize));
+            expr = expr.substr(arrBigSize);
+        }
+    }
+    for(let i = 0; i < arrBig.length; i++) {
+
+        if( i == 0) {
+            arr.push(arrBig[i]);
+        } else {
+            arr.splice(0, 1, arrBig[i]);3
+        }
+
+        let arrPrew = [];
+        
+        arr.forEach(element => {
+            while (element) {
+                if(element == '**********') { 
+                    arrPrew.push(" ");
+                    break;
+                }
+                if (element.length < arrSmallSize) {
+                    arrPrew.push(element);
+                    break;
+                } else {
+                    if(element.substr(0, arrSmallSize) == '10') {
+                        arrPrew.push(".");
+                    } else if(element.substr(0, arrSmallSize) == '11') {
+                        arrPrew.push("-");
+                    } else {}
+                    element = element.substr(arrSmallSize);
+                }
+            }
+            arrSmall.push(arrPrew);
+        });
+    }
+    for(let i =0; i < arrSmall.length; i++) {
+        if(MORSE_TABLE[arrSmall[i].join('')] == undefined) {
+            letter += ' ';
+        } else {
+            letter += MORSE_TABLE[arrSmall[i].join('')];
+        }
+    }
+    return letter;
 }
 
 module.exports = {
