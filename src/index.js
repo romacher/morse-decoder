3d7d884086ef2238d1d135de9685288fdfa983fa
Yana Gurinovich <yana.gurinovich@instatsport.com>
2020-10-21 18:27:09 +0300

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,11 +35,40 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    
+        let newArray = [];  
+        for(let i=0; i < expr.length; i+=10) {
+        newArray.push(expr.slice(i, i+10));
+        }      
+        let array = []; 
+      for(let n=0; n < newArray.length; n++){
+        let strInArr = '';
+        for(let j=0; j < newArray[n].length; j+=2){    
+          
+          if( newArray[n][j] + newArray[n][j+1] == '10'){
+            strInArr += '.';
+          }
+          if( newArray[n][j] + newArray[n][j+1] == '11'){
+            strInArr += '-';          
+        } 
+         if( newArray[n] === '**********'){
+            strInArr = ' ';          
+        }       
+      }
+        array.push(strInArr);      
+    }
+  
+      let outputString ='';
+     for(let k =0; k < array.length; k++){
+    outputString += MORSE_TABLE[array[k]];
+    }
+    return outputString;
+  }
+
 
 module.exports = {
     decode
