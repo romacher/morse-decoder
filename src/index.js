f827c3b7cc70acf3b24031449b5e39d90b59767d
Harleycat80 <Harleycat@rambler.ru>
2022-01-30 18:19:29 +0400

    done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr =[]
+    let str =""
+    let str2 =""
+    let arr3 =[]
+    let arr2=[]
+    let str3=''
+    let chank =10
+    for(let i=0; i< expr.length; i+=chank){
+    str = expr.slice(i,i+chank)
+    str2 =str.slice(str.indexOf("1"))
+    arr.push(str2)
+    str=0
+    str2=""} 
+    for (let i=0; i<arr.length;i++){
+        for(let j=0; j <= arr[i].length; j+=2){
+            if(arr[i].slice(j,j+2)=="11"){
+                str3+="-"}
+            else if(arr[i].slice(j,j+2)=="10"){
+                str3+="."}
+            else if(arr[i]=="*"){
+                str3="*"
+            }
+        }
+        arr2.push(str3) 
+        str3=''
+    }
+        for (let i =0; i<arr2.length; i++){
+            if(arr2[i]=="*"){
+                arr3.push(' ')
+            }
+            else {arr2[i] = MORSE_TABLE[arr2[i]] 
+            arr3.push(arr2[i])
+            } 
+        }
+    return arr3.join('').toString()
+}           
+
+   
 
 module.exports = {
     decode
