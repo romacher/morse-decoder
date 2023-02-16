ab1a747548315d9f981bd6ccc7fd33ac5e62bca1
SergeyBrutskey <brutskey1994@mail.ru>
2023-02-17 01:05:07 +0300

    solutione morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = ""
+    for (let i = 0, j = 10; i < expr.length; i = i + 10, j = j + 10) {
+        let Number = "";
+        Number = expr.slice(i, j);
+        let morse = "";
+        if (Number === "**********") {
+            morse = morse + " ";
+        } else {
+            for (let a = 0, b = 2; a < 10; a = a + 2, b = b + 2) {
+                if(Number.slice(a, b) === "10") {
+                    morse = morse + ".";
+                }
+                else if(Number.slice(a, b) === "11") {
+                    morse = morse + "-";
+                }
+            }  
+        }
+        if (morse === " ") {
+            result = result + " ";
+        } else {
+            result = result + MORSE_TABLE[morse]
+        }
+    }
+    return result
+    }
 
 module.exports = {
     decode
